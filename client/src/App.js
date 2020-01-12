20aeddd622033af3ae02bd767f44cdf75f4410ff
Ganesh Sankaran <ganesh0sankaran@gmail.com>
2020-01-11 19:27:10 -0800

    At least we got the buttons working

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
-  getData() {
-    var xhr = new XMLHttpRequest()
 
-    // get a callback when the server responds
-    xhr.addEventListener('load', () => {
-      // update the state of the component with the result here
-      console.log(xhr.responseText);
-    })
-    // open the request with the verb and the url
-    xhr.open('GET', 'http://localhost:3000/hello');
-    // send the request
-    xhr.send();
+  async getData() {
+    const response =
+    await axios.get("http://localhost:3000/hello")
+    alert(response.data);
   }
 
   render() {
     return (
+      <>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
       <div className="App">
         <header className="App-header">
-          <p>Hello World!</p>
+          <button onClick={this.getData}>
+            Create
+          </button>
+          <button onClick={this.getData}>
+            Join
+          </button>
         </header>
       </div>
+      </>
     );
   }
 }
--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+var cors = require('cors');
 var express = require('express');
 var app = express();
+app.use(cors());
 
 app.get('/hello', function(req, res) {
   res.send("hello world");
